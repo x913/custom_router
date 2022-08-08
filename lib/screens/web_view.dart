3f4c177fe1f8082d48dd32368f3a0873a0fe780b
x913 <x913x913@gmail.com>
2022-08-08 21:40:19 +0500

    fix ua

--- lib/screens/web_view.dart
+++ lib/screens/web_view.dart
@@ -50,7 +50,7 @@ class _WebViewPageState extends State<WebViewPage> {
       var version = "Version/13.0.3";
       var safari = "Safari/604.1";
   
-      if(index == -11) {
+      if(index != -11) {
 	print("AAA no mobile string found");
         browserUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";  
       } else {
