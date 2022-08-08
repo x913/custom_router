2d27bc99a006e9db5fbb37a916c764f250a66c7c
x913 <x913x913@gmail.com>
2022-08-08 18:58:17 +0500

    add safari custom useragent

--- lib/screens/web_view.dart
+++ lib/screens/web_view.dart
@@ -59,6 +59,7 @@ class _WebViewPageState extends State<WebViewPage> {
                 WebView(
                   gestureNavigationEnabled: true,
                   initialUrl: webViewUrl,
+		  userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
                   javascriptMode: JavascriptMode.unrestricted,
                   onWebViewCreated: (con) {
                     print("AAA creation completed url is ${webViewUrl}");
